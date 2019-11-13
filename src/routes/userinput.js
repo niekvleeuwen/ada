07d75aa89b4d8de6cd4158bb8435be0a0ab1be03
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2019-11-13 23:42:24 +0100

    Added POST route for userinput

--- src/routes/userinput.js
+++ src/routes/userinput.js
@@ -6,4 +6,16 @@ router.get('/', function (req, res, next) {
     res.json({code: '400', error: 'No parameters found' });
 })
 
+/* POST user input listing. */
+router.post('/', function (req, res, next) {
+    /* Expected params: token, textinput */
+    if(req.body.token == null || req.body.textinput == null){
+        res.json({code: '400', error: 'No parameters found' });
+    }else{
+        //call the NLP here
+        res.json({code: '200', error: 'The answer you are looking for...' });
+    }
+})
+
+
 module.exports = router
