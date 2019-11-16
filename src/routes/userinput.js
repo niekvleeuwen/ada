fb802d14978e3edaa0175b4ba332b44cca5d1ed3
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2019-11-16 14:17:21 +0100

    Added more useful responses on errors

--- src/routes/userinput.js
+++ src/routes/userinput.js
@@ -8,12 +8,14 @@ router.get('/', function (req, res, next) {
 
 /* POST user input listing. */
 router.post('/', function (req, res, next) {
-    /* Expected params: token, textinput */
-    if(req.body.token == null || req.body.textinput == null){
-        res.json({code: '400', error: 'No parameters found' });
+    /* Expected params: token, textInput */
+    if(req.body.token == null){
+        res.json({code: '400', error: 'Token not entered' });
+    }else if(req.body.textInput == null){
+        res.json({code: '400', error: 'TextInput not entered' });
     }else{
         //call the NLP here
-        res.json({code: '200', error: 'The answer you are looking for...' });
+        res.json({code: '200', response: 'The answer you are looking for...' });
     }
 })
 
